e27982be79384db44538369795986569905f2f51
danielpowell4 <danielpowell12@msn.com>
2015-11-16 20:05:56 -0700

    using replace to make sure everyone knows how cool Julia is

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,9 @@
-$('#main').append([yourName])
+$('#main').append("Julia")
+
+var awesomeThoughts = "My name is Julia and I am AWESOME";
+
+$('#main').append(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN")
+
+$('#main').append(funThoughts);
